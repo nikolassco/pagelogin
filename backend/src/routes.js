aa4752fe207956bc44d30968b0a7c16d22cdeeda
Nikolas Oliveira <60801016+nikolassco@users.noreply.github.com>
2023-07-14 17:12:38 -0300

    Delete backend directory

--- backend/src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { registerUser, login } = require('./controllers/users');
-const routes = express();
-
-routes.post('/register', registerUser);
-routes.post('/login', login);
-
-module.exports = routes;
